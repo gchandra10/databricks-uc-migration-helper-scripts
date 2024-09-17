ee2799b2c2b679ccf4786852d839ee4ead5776b5
gchandra@gmail.com <gchandra@gmail.com>
2024-09-17 20:38:27 +0000

    Version 1.6

--- identify-tables-with-same-location.py
+++ identify-tables-with-same-location.py
@@ -5,6 +5,11 @@
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC **Note:** If there are tables without valid location then this will display the error message and keep a log of all those errors in log file.
+
+# COMMAND ----------
+
 # DBTITLE 1,Import Libraries
 from typing import List,Dict
 from pyspark.sql.functions import col
@@ -12,7 +17,7 @@ from pyspark.sql.functions import col
 # Custom Py file
 from common_functions import logger_setup
 
-app_name = "identify-tables-with-same-location"
+app_name = "identify-db-tables-with-same-location"
 logger,file_handler = logger_setup(app_name)
 
 # COMMAND ----------
@@ -49,8 +54,7 @@ def get_all_databases() -> List[str]:
         logger.info("Executing get_all_databases function")
         
         spark.sql("use catalog hive_metastore")
-        #return spark.catalog.listDatabases()
-        return [spark.catalog.getDatabase("uc_demo")]
+        return spark.catalog.listDatabases()
     except Exception as e:
         logger.error(e)
 
@@ -71,7 +75,8 @@ def get_table_mount_location():
     lst_final_data = []
     try:
         for database in all_databases:
-            logger.info(f"Collecting data from hive_metastore.{database.name}")
+            database_name = database.name.replace("`", "")
+            logger.info(f"Collecting data from hive_metastore.{database_name}")
 
             lst_tables = spark.sql(f"SHOW TABLES IN hive_metastore.{database.name}").collect()
             try:
@@ -80,14 +85,17 @@ def get_table_mount_location():
                         ## Skip __apply tables created by DLT
                         if not t.tableName.startswith('__'):
                             try:
-                                sql = f"describe extended hive_metastore.`{database.name}`.`{t.tableName}`"
+                                table_name = t.tableName.replace("`", "")
+                                sql = f"describe extended hive_metastore.`{database_name}`.`{table_name}`"
                                 extended_desc = spark.sql(sql)
+
+                                owner = extended_desc.filter(extended_desc["col_name"]=="Owner").collect()
+                                owner_name = owner[0]['data_type'] if owner and 'data_type' in owner[0] else "None"
 
                                 location_info = extended_desc.filter(extended_desc["col_name"]=="Location").collect()
                                 if location_info:
                                     location = location_info[0]["data_type"]
-                                    row = (database.name,t.tableName,location)
-
+                                    row = (database_name,table_name,location,owner_name)
                                     lst_final_data.append(row)
                             except Exception as e:
                                 logger.error(e)
@@ -96,7 +104,7 @@ def get_table_mount_location():
             except Exception as e:
                 logger.error(e)
 
-        columns = ["database_name","table_name","location"]
+        columns = ["database_name","table_name","location","owner_name"]
 
         if len(lst_final_data) > 0:
             df = spark.createDataFrame(lst_final_data,columns)
@@ -122,7 +130,7 @@ if __name__ == "__main__":
         
         # Filter where count of location is greater than 1
         df_result = df_with_counts.filter(col("location_count") > 1) \
-                       .select("database_name", "table_name", "location", "location_count")
+                       .select("database_name", "table_name", "owner_name", "location", "location_count")
 
         display(df_result)
 
@@ -133,3 +141,7 @@ if __name__ == "__main__":
 
 file_handler.close()
 logger.removeHandler(file_handler)
+
+# COMMAND ----------
+
+
