ce2554cbc5d56ebef06eff2ffa897f911611219c
gchandra@gmail.com <gchandra@gmail.com>
2024-09-17 15:05:12 +0000

    Identify Tables with Same Location

--- identify-tables-with-same-location.py
+++ identify-tables-with-same-location.py
@@ -1,11 +1,7 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC
-# MAGIC Using **Azure cloud**, use this to determine how many databases, tables, mounts are using wasbs:// protocol.
-# MAGIC
-# MAGIC - This can be used prior to running UCX assessment.
-# MAGIC - If wasbs:// protocol is used, create equivalent containers / storage locations using abfss:// and copy the data from wasbs:// to abfss://
-# MAGIC - *azcopy* tool works better. In case of smaller datasets, read the data from wasbs:// using SPARK and write to abfss://
+# MAGIC In HMS world its common to create multiple tables pointing to the same External location. This script will help to identify those tables.
 
 # COMMAND ----------
 
@@ -16,7 +12,7 @@ from pyspark.sql.functions import col
 # Custom Py file
 from common_functions import logger_setup
 
-app_name = "azure_get_location_protocol"
+app_name = "identify-tables-with-same-location"
 logger,file_handler = logger_setup(app_name)
 
 # COMMAND ----------
@@ -27,7 +23,7 @@ logger,file_handler = logger_setup(app_name)
 def get_mount_points() -> Dict[str, str]:
     """
         This function returns list of Mount Point names along with Cloud Location
-        /mnt/mountname  abfs://cloud_path
+        /mnt/mountname  abfs://cloud_path  s3://
     """
     try:
         logger.info("Executing get_mount_points function")
@@ -53,7 +49,8 @@ def get_all_databases() -> List[str]:
         logger.info("Executing get_all_databases function")
         
         spark.sql("use catalog hive_metastore")
-        return spark.catalog.listDatabases()
+        #return spark.catalog.listDatabases()
+        return [spark.catalog.getDatabase("uc_demo")]
     except Exception as e:
         logger.error(e)
 
@@ -62,16 +59,11 @@ def get_all_databases() -> List[str]:
 # DBTITLE 1,Get Table Mount Location
 def get_table_mount_location():
     """
-        This function returns all the Tables from each database along with mount location and adls_prefix
-        The adls_prefix can be wasbs or abfss.
-        Unity Catalog doesn't support wasbs.
+        This function returns all the Tables from each database along with location
     """
     logger.info("Executing get_table_mount_location function")
 
-    # Collect all the mount points as a Dictionary
-    # The tables created will have Mount Point location /mnt/somename
-    # This function will help to replace the /mnt/somename with equivalent ADLS location
-    mount_dict = get_mount_points()
+    # mount_dict = get_mount_points()
 
     # Collect all databases as List
     all_databases = get_all_databases()
@@ -79,31 +71,22 @@ def get_table_mount_location():
     lst_final_data = []
     try:
         for database in all_databases:
-            #print(dbs.name)
-            #spark.sql(f"use hive_metastore.{database.name}")
             logger.info(f"Collecting data from hive_metastore.{database.name}")
 
             lst_tables = spark.sql(f"SHOW TABLES IN hive_metastore.{database.name}").collect()
-            
             try:
                 for t in lst_tables:
                     if t.isTemporary is False:
                         ## Skip __apply tables created by DLT
                         if not t.tableName.startswith('__'):
-                            
                             try:
                                 sql = f"describe extended hive_metastore.`{database.name}`.`{t.tableName}`"
                                 extended_desc = spark.sql(sql)
 
                                 location_info = extended_desc.filter(extended_desc["col_name"]=="Location").collect()
-                                
                                 if location_info:
                                     location = location_info[0]["data_type"]
-                                    loc_parts = location.split('/')
-                                    # retrieving the first 2 part of the path /mnt/mountname
-                                    required_path = '/'+ '/'.join(loc_parts[1:3])
-
-                                    row = (database.name,t.tableName,required_path, mount_dict.get(required_path))
+                                    row = (database.name,t.tableName,location)
 
                                     lst_final_data.append(row)
                             except Exception as e:
@@ -113,7 +96,8 @@ def get_table_mount_location():
             except Exception as e:
                 logger.error(e)
 
-        columns = ["database_name","table_name","location","adls_prefix"]
+        columns = ["database_name","table_name","location"]
+
         if len(lst_final_data) > 0:
             df = spark.createDataFrame(lst_final_data,columns)
 
@@ -125,17 +109,23 @@ def get_table_mount_location():
 # COMMAND ----------
 
 # DBTITLE 1,Main Routine
+from pyspark.sql.window import Window
+from pyspark.sql.functions import count, col
+
 if __name__ == "__main__":
     try:
         df = get_table_mount_location()
 
-        # Filter wasbs protocols
-        df_filter = df.filter(col("adls_prefix") == "wasbs")
+        window_spec = Window.partitionBy("location") 
+        # Add a count column over the window specification
+        df_with_counts = df.withColumn("location_count", count("location").over(window_spec))
+        
+        # Filter where count of location is greater than 1
+        df_result = df_with_counts.filter(col("location_count") > 1) \
+                       .select("database_name", "table_name", "location", "location_count")
 
-        if df_filter.count() > 0:
-            print("wasbs protocols are not supported by Unity Catalog, please move the data to abfss:// and remount wasbs:// to abfss://. Migration will EXIT now.")
-            display(df_filter)
-            exit
+        display(df_result)
+
     except Exception as e:
         logger.error(e)
 
@@ -143,7 +133,3 @@ if __name__ == "__main__":
 
 file_handler.close()
 logger.removeHandler(file_handler)
-
-# COMMAND ----------
-
-
